6c4a345ab896dd9a3cf9b1eab89816bd00cfe45d
huqijian <1571149173@qq.com>
2024-05-10 00:44:30 +0800

    创建项目

---
+++ src/main/java/com/hqj/Main.java
@@ -0,0 +1,16 @@
+package com.hqj;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.hqj
+ * Description:
+ *
+ * @Author:HQJ
+ * @Create:2024/5/10 - 0:07
+ * @Version v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
